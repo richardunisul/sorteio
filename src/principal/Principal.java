7aa368e519e73f454ad1d82d8d933616d5c98ebb
richardunisul <103585524+richardunisul@users.noreply.github.com>
2024-04-23 17:25:31 -0300

    Add files via upload

---
+++ src/principal/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+
+import view.TelaPrincipal;
+
+/**
+ *
+ * @author RIHS
+ */
+public class Principal {
+     public static void main(String[] args) {
+       TelaPrincipal objetotela = new TelaPrincipal();// Cria objeto do JFrame TelaPrincipal
+       objetotela.setVisible(true);
+    }
+}
